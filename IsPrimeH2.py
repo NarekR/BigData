23e631e7ef83850c63262843b6f8c631d1964ab9
Narek <90374805+NarekR@users.noreply.github.com>
2023-10-24 18:01:07 +0400

    Create IsPrimeH2.py

---
+++ IsPrimeH2.py
@@ -0,0 +1,24 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+
+    #i = 6k-1
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+n = int(input("Введите до сколки надо шитать: "))
+
+    
+for i in range(1, n):
+    if is_prime(i):
+        print(i)
+
